efc5da5b6b8c95e89c7da15bb702a19774e52d90
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-01 15:35:47 +0300

    Dtos added

--- Finance.API/Dtos/Comment/StockCommentDto.cs
+++ Finance.API/Dtos/Comment/StockCommentDto.cs
@@ -1,10 +1,10 @@
 ﻿namespace Finance.API.Dtos.Comment
 {
-	public class CreateCommentRequestDto
+	public class StockCommentDto
 	{
+		public int Id { get; set; }
 		public string Title { get; set; } = string.Empty;
 		public string Content { get; set; } = string.Empty;
-		public int? StockId { get; set; }
-
+		public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
 	}
 }
