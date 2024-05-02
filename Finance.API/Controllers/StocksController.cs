f003695b5efbcf7eb2d5179b67b2252b3b9e5088
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-02 15:27:11 +0300

    refactor and filtering

--- Finance.API/Controllers/StocksController.cs
+++ Finance.API/Controllers/StocksController.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using Finance.API.Data;
 using Finance.API.Dtos.Stock;
+using Finance.API.Helpers;
 using Finance.API.Interfaces;
 using Finance.API.Models;
 using Finance.API.Repositories;
@@ -10,78 +11,90 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Finance.API.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class StocksController : ControllerBase
-    {
-        private readonly AppDbContext _context;
-        private readonly IMapper _mapper;
-        private readonly IStockRepository _stockRepository;
-        public StocksController(AppDbContext context, IMapper mapper, IStockRepository stockRepository)
-        {
-            _context = context;
-            _mapper = mapper;
-            _stockRepository = stockRepository;
-        }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class StocksController : ControllerBase
+	{
+		private readonly AppDbContext _context;
+		private readonly IMapper _mapper;
+		private readonly IStockRepository _stockRepository;
+		public StocksController(AppDbContext context, IMapper mapper, IStockRepository stockRepository)
+		{
+			_context = context;
+			_mapper = mapper;
+			_stockRepository = stockRepository;
+		}
 
 
 		[HttpGet("all")]
-		public async Task<IActionResult> GetAll()
-        {
-            var stocks = await _stockRepository.GetAllAsync();
-            var stockDtos = _mapper.Map<List<StockDto>>(stocks);
-            return Ok(stockDtos);
-        }
+		public async Task<IActionResult> GetAll([FromQuery] QueryObject queryObject)
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
+
+			var stocks = await _stockRepository.GetAllAsync(queryObject);
+			var stockDtos = _mapper.Map<List<StockDto>>(stocks);
+			return Ok(stockDtos);
+		}
 
 		[HttpGet("allwithcomments")]
 		public async Task<IActionResult> GetAllWithComments()
-        {
-            var stocks = await _stockRepository.GetAllWithCommentsAsync();
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
+
+			var stocks = await _stockRepository.GetAllWithCommentsAsync();
 			var stockDtos = _mapper.Map<List<StockDto>>(stocks);
 			return Ok(stockDtos);
 		}
 
 
 		// FromRoute https://api.myservice.com/books/123
-		[HttpGet("{id}")]
-        public async Task<IActionResult> GetById([FromRoute] int id)
-        {
-            var stock = await _stockRepository.GetByIdAsync(id);
-            if (stock == null) return NotFound();
-            var stockDto = _mapper.Map<StockDto>(stock);
-            return Ok(stockDto);
+		[HttpGet("{id:int}")]
+		public async Task<IActionResult> GetById([FromRoute] int id)
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
 
-        }
+			var stock = await _stockRepository.GetByIdAsync(id);
+			if (stock == null) return NotFound();
+			var stockDto = _mapper.Map<StockDto>(stock);
+			return Ok(stockDto);
 
-
-        [HttpPost]
-        public async Task<IActionResult> CreateStock([FromBody] CreateStockRequestDto stockRequestDto)
-        {
-            var stockModel = _mapper.Map<Stock>(stockRequestDto);
-            await _stockRepository.CreateAsync(stockModel);
-            return Ok(stockRequestDto);
-        }
+		}
 
 
-        [HttpPut]
-        [Route("{id}")]
-        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockRequestDto)
-        {
-            var stockModel = await _stockRepository.UpdateAsync(id, updateStockRequestDto);
-            if (stockModel == null) return NotFound();
-            return Ok(updateStockRequestDto);
+		[HttpPost]
+		public async Task<IActionResult> CreateStock([FromBody] CreateStockRequestDto stockRequestDto)
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
 
-            //_mapper.Map<Stock>(updateStockRequestDto); değişkene atamadığım için kullanmıyorum
-            //_mapper.Map(updateStockRequestDto, stockModel);
-        }
+			var stockModel = _mapper.Map<Stock>(stockRequestDto);
+			await _stockRepository.CreateAsync(stockModel);
+			return Ok(stockRequestDto);
+		}
 
-        [HttpDelete]
-        [Route("{id}")] 
-        public async Task<IActionResult> Delete([FromRoute] int id)
-        {
-            var stockModel = await _stockRepository.DeleteAsync(id);
-            if (stockModel == null) return NotFound();
-            return NoContent();
-        }
-    }
+
+		[HttpPut]
+		[Route("{id:int}")]
+		public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockRequestDto)
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
+
+			var stockModel = await _stockRepository.UpdateAsync(id, updateStockRequestDto);
+			if (stockModel == null) return NotFound();
+			return Ok(updateStockRequestDto);
+
+			//_mapper.Map<Stock>(updateStockRequestDto); değişkene atamadığım için kullanmıyorum
+			//_mapper.Map(updateStockRequestDto, stockModel);
+		}
+
+		[HttpDelete]
+		[Route("{id:int}")]
+		public async Task<IActionResult> Delete([FromRoute] int id)
+		{
+			if (!ModelState.IsValid) return BadRequest(ModelState);
+
+			var stockModel = await _stockRepository.DeleteAsync(id);
+			if (stockModel == null) return NotFound();
+			return NoContent();
+		}
+	}
 }
