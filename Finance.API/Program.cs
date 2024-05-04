5bd9740f4a0e58fa90183f990f73cc0d980e6156
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-04 14:44:49 +0300

    Configured Identity, Authentication, and Jwt settings

--- Finance.API/Program.cs
+++ Finance.API/Program.cs
@@ -1,8 +1,13 @@
 using Finance.API.Data;
 using Finance.API.Interfaces;
 using Finance.API.Mappers;
+using Finance.API.Models;
 using Finance.API.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,7 +21,40 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+	opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlCon"));
+});
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(opt =>
+{
+	opt.Password.RequireDigit = true;
+	opt.Password.RequireLowercase = true;
+	opt.Password.RequireUppercase = true;
+	opt.Password.RequireNonAlphanumeric = true;
+	opt.Password.RequiredLength = 6;
+}).AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication(opt =>
+{
+
+	opt.DefaultAuthenticateScheme =
+	opt.DefaultChallengeScheme =
+	opt.DefaultForbidScheme =
+	opt.DefaultScheme =
+	opt.DefaultSignInScheme =
+	opt.DefaultSignOutScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(opt =>
+{
+	opt.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidIssuer = builder.Configuration["JWT:Issuer"],
+		ValidateAudience = true,
+		ValidAudience = builder.Configuration["JWT:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(
+			Encoding.UTF8.GetBytes(builder.Configuration["JWT:SigningKey"])
+			)
+	};
 });
 
 builder.Services.AddScoped<IStockRepository, StockRepository>();
@@ -27,12 +65,12 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	app.UseSwagger();
+	app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
