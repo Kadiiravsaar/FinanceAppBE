c6184c8a3e458c1b6ed91f5131fb852bc9edc69c
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-04 14:43:35 +0300

    Replaced DbContext with IdentityDbContext in Efcore

--- Finance.API/Data/AppDbContext.cs
+++ Finance.API/Data/AppDbContext.cs
@@ -1,9 +1,10 @@
 ﻿using Finance.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finance.API.Data
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext
     {
         public AppDbContext(DbContextOptions dbContextOptions) : base(dbContextOptions)
         {
