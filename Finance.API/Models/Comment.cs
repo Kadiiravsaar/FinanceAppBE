e94f72167c44a0bc61f4005b9315522fa8107ba7
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-07 19:17:08 +0300

    AppUserId field added to Comment Class

--- Finance.API/Models/Comment.cs
+++ Finance.API/Models/Comment.cs
@@ -6,8 +6,9 @@
         public string Title { get; set; } = string.Empty;
         public string Content { get; set; } = string.Empty;
         public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
-
         public int? StockId { get; set; }
         public Stock? Stock{ get; set; }
-    }
+		public string? AppUserId { get; set; }
+		public AppUser AppUser { get; set; }
+	}
 }
