e11a8ac75cdd641862dbb01e6bdb5889b61521e2
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-08 15:44:19 +0300

    refactor

--- Finance.API/Dtos/Stock/CreateStockRequestDto.cs
+++ Finance.API/Dtos/Stock/CreateStockRequestDto.cs
@@ -8,7 +8,7 @@ namespace Finance.API.Dtos.Stock
 		[MaxLength(10, ErrorMessage = "Symbol cannot be over 10 over characters")]
 		public string Symbol { get; set; } = string.Empty;
 		[Required]
-		[MaxLength(10, ErrorMessage = "Company Name cannot be over 10 over characters")]
+		[MaxLength(50, ErrorMessage = "Company Name cannot be over 50 over characters")]
 		public string CompanyName { get; set; } = string.Empty;
 		[Required]
 		[Range(1, 1000000000)]
@@ -17,7 +17,7 @@ namespace Finance.API.Dtos.Stock
 		[Range(0.001, 100)]
 		public decimal LastDiv { get; set; }
 		[Required]
-		[MaxLength(10, ErrorMessage = "Industry cannot be over 10 characters")]
+		[MaxLength(50, ErrorMessage = "Industry cannot be over 50 characters")]
 		public string Industry { get; set; } = string.Empty;
 		[Range(1, 5000000000)]
 		public long MarketCap { get; set; }
