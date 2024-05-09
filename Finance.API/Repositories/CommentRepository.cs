7d9c37e93dadb4629d6bf33eb297aed1f9f04d40
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-09 13:44:31 +0300

    Comment Include (for AppUser)

--- Finance.API/Repositories/CommentRepository.cs
+++ Finance.API/Repositories/CommentRepository.cs
@@ -2,8 +2,10 @@
 using Finance.API.Data;
 using Finance.API.Dtos.Comment;
 using Finance.API.Dtos.Stock;
+using Finance.API.Extensions;
 using Finance.API.Interfaces;
 using Finance.API.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finance.API.Repositories
@@ -12,16 +14,26 @@ namespace Finance.API.Repositories
 	{
 		private readonly AppDbContext _context;
 		private readonly IMapper _mapper;
+		private readonly IHttpContextAccessor _httpContextAccessor;
+		private readonly UserManager<AppUser> _userManager;
 
 
-		public CommentRepository(AppDbContext context, IMapper mapper)
+
+
+		public CommentRepository(AppDbContext context, IMapper mapper, IHttpContextAccessor httpContextAccessor, UserManager<AppUser> userManager)
 		{
 			_context = context;
 			_mapper = mapper;
+			_httpContextAccessor = httpContextAccessor;
+			_userManager = userManager;
 		}
 
 		public async Task<Comment> CreateAsync(Comment comment)
 		{
+			var userId = _httpContextAccessor.HttpContext.User.GetUserId();
+			var appUser = await _userManager.FindByIdAsync(userId);
+			comment.AppUserId = appUser.Id;
+			//comment.AppUser.UserName = appUser.UserName;
 			await _context.Comments.AddAsync(comment);
 			await _context.SaveChangesAsync();
 			return comment;
@@ -38,12 +50,12 @@ namespace Finance.API.Repositories
 
 		public async Task<List<Comment>> GetAllAsync()
 		{
-			return await _context.Comments.ToListAsync();
+			return await _context.Comments.Include(c=>c.AppUser).ToListAsync();
 		}
 
 		public async Task<Comment> GetByIdAsync(int id)
 		{
-			var commentId = await _context.Comments.FindAsync(id);
+			var commentId = await _context.Comments.Include(c => c.AppUser).FirstOrDefaultAsync(c=>c.Id == id);
 			return commentId;
 
 		}
