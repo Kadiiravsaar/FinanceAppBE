ff2f769057d5812cb6c03cf0352092b0f8744e06
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-13 20:05:02 +0300

    refactor

--- Finance.Service/Mapping/MapProfile.cs
+++ Finance.Service/Mapping/MapProfile.cs
@@ -1,4 +1,6 @@
 ﻿using AutoMapper;
+using Finance.Core.DTOs.Comment;
+using Finance.Core.DTOs.Result;
 using Finance.Core.DTOs.Stock;
 using Finance.Core.DTOs.User;
 using Finance.Core.Models;
@@ -16,18 +18,18 @@ namespace Finance.Service.Mapping
         {
             CreateMap<Stock, StockDto>();
             CreateMap<FMPStock, Stock>();
-            CreateMap<StockDto, List<StockCommentDto>>();
+
             CreateMap<Comment, StockCommentDto>();
             CreateMap<CreateStockRequestDto, Stock>().ReverseMap();
             CreateMap<UpdateStockRequestDto, Stock>().ReverseMap();
-            //CreateMap<CreateCommentRequestDto, CommentDto>();
+            CreateMap<CreateCommentRequestDto, CommentDto>();
 
 
             CreateMap<AppUser, UserDto>();
 
-            //CreateMap<CreateCommentRequestDto, Comment>();
-            //CreateMap<Comment, CommentDto>()
-            //	.ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.AppUser.UserName)); // Yeni alanın eşlenmesi
+            CreateMap<CreateCommentRequestDto, Comment>();
+            CreateMap<Comment, CommentDto>()
+            	.ForMember(dest => dest.CreatedBy, opt => opt.MapFrom(src => src.AppUser.UserName)); // Yeni alanın eşlenmesi
 
 
         }
