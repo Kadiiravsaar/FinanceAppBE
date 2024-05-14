1a6dc4504687f308b8ced3f214b1c2a397566704
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-14 15:24:43 +0300

    Comment Completed interfaces for Comment operations

--- Finance.Core/Repositories/ICommentRepository.cs
+++ Finance.Core/Repositories/ICommentRepository.cs
@@ -1,11 +1,14 @@
-﻿using Finance.Core.Models;
+﻿using Finance.Core.DTOs.Comment;
+using Finance.Core.DTOs.Result;
+using Finance.Core.Models;
 
 namespace Finance.Core.Repositories
 {
 	public interface ICommentRepository : IGenericRepository<Comment>
 	{
+		Task<Comment> CreateAsync(string symbol, Comment comment);
+
+		Task<List<Comment>> GetAllWithUserAsync();
+
 	}
-
-
-	
 }
--- Finance.Core/Services/ICommentService.cs
+++ Finance.Core/Services/ICommentService.cs
@@ -1,10 +1,20 @@
-﻿using Finance.Core.Models;
+﻿using Finance.Core.DTOs.Comment;
+using Finance.Core.DTOs.Result;
+using Finance.Core.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Finance.Core.Services
 {
 	public interface ICommentService : IService<Comment>
 	{
+		Task<CustomResponseDto<CommentDto>> CreateAsync(string symbol,CreateCommentRequestDto createCommentRequestDto);
+		Task<CustomResponseDto<List<CommentDto>>> GetAllWithUserAsync();
 
+
+		//public async Task<List<Comment>> GetAllAsync()
+		//{
+		//	return await _context.Comments.Include(c => c.AppUser).ToListAsync();
+		//}
 
 	}
 
