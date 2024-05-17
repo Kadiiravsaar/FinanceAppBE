57c979cb185ba4c2bf0f57455dcd01a9b8eaf187
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-17 21:49:40 +0300

    UseCustomExceptionHandler Added UseCustomExceptionHandler middleware

---
+++ Finance.API/Middlewares/UseCustomExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Finance.Core.DTOs.Result;
+using Finance.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Text.Json;
+
+namespace Finance.API.Middlewares
+{
+	public static class UseCustomExceptionHandler
+	{
+		public static void UserCustomException(this IApplicationBuilder app)
+		{
+			app.UseExceptionHandler(config =>
+			{
+				config.Run(async context => // Eğer Run fonksiyonuna girerse buradan ileriye gitmez ve geriye döner
+				{
+					context.Response.ContentType = "application/json"; // Response olarak karşı tarafa ne döneceğimi belirtir
+
+					var exceptionFeature = context.Features.Get<IExceptionHandlerFeature>(); // Hatanın detaylarını alır
+
+					var statusCode = exceptionFeature.Error switch
+					{
+						ClientSideException => 400,
+						NotFoundException => 404,
+						_ => 500
+					};
+
+					context.Response.StatusCode = statusCode;
+
+					var response = CustomResponseDto<NoContentDto>.Fail(statusCode, exceptionFeature.Error.Message);
+
+					await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+
+				});
+			});
+		}
+	}
+}
