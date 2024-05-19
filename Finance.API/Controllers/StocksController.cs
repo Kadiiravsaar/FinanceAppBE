6e769a3587100860593aaa79ab11768d949856c3
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-19 23:16:34 +0300

    Updated StocksController class: - Removed AppDbContext dependency. - Used StockService for stock operations. - Improved code structure and readability.

--- Finance.API/Controllers/StocksController.cs
+++ Finance.API/Controllers/StocksController.cs
@@ -1,74 +1,60 @@
 ﻿using AutoMapper;
-using Finance.API.Data;
-using Finance.API.Dtos.Stock;
-using Finance.API.Helpers;
-using Finance.API.Interfaces;
-using Finance.API.Models;
-using Finance.API.Repositories;
-using Microsoft.AspNetCore.Http;
+using Finance.Core.DTOs.Result;
+using Finance.Core.DTOs.Stock;
+using Finance.Core.Models;
+using Finance.Core.Services;
+using Finance.Repository.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace Finance.API.Controllers
 {
-	[Route("api/[controller]")]
+    [Route("api/[controller]")]
 	[ApiController]
 	public class StocksController : ControllerBase
 	{
-		private readonly AppDbContext _context;
 		private readonly IMapper _mapper;
-		private readonly IStockRepository _stockRepository;
-		public StocksController(AppDbContext context, IMapper mapper, IStockRepository stockRepository)
+		private readonly IStockService _service;
+
+		public StocksController(IMapper mapper, IStockService stockService)
 		{
-			_context = context;
 			_mapper = mapper;
-			_stockRepository = stockRepository;
+			_service = stockService;
 		}
 
-
-		[HttpGet("all")]
-		public async Task<IActionResult> GetAll([FromQuery] QueryObject queryObject)
+		[HttpGet]
+		public async Task<IActionResult> All()
 		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
-
-			var stocks = await _stockRepository.GetAllAsync(queryObject);
-			var stockDtos = _mapper.Map<List<StockDto>>(stocks);
-			return Ok(stockDtos);
+			var stocks = await _service.GetAllAsync();
+			var stocksDtos = _mapper.Map<List<StockDto>>(stocks.ToList());
+			return Ok(CustomResponseDto<List<StockDto>>.Success(200, stocksDtos));
 		}
+
 
 		[HttpGet("allwithcomments")]
 		public async Task<IActionResult> GetAllWithComments()
 		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
-
-			var stocks = await _stockRepository.GetAllWithCommentsAsync();
-			var stockDtos = _mapper.Map<List<StockDto>>(stocks);
-			return Ok(stockDtos);
-		}
-
-
-		// FromRoute https://api.myservice.com/books/123
-		[HttpGet("{id:int}")]
-		public async Task<IActionResult> GetById([FromRoute] int id)
-		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
-
-			var stock = await _stockRepository.GetByIdAsync(id);
-			if (stock == null) return NotFound();
-			var stockDto = _mapper.Map<StockDto>(stock);
-			return Ok(stockDto);
-
+			var stocks = await _service.GetAllWithCommentsAsync();
+			var stockDtos = _mapper.Map<List<StockDto>>(stocks.Data);
+			return Ok(CustomResponseDto<List<StockDto>>.Success(200, stockDtos));
 		}
 
 
 		[HttpPost]
-		public async Task<IActionResult> CreateStock([FromBody] CreateStockRequestDto stockRequestDto)
+		public async Task<IActionResult> AddStock([FromBody] CreateStockRequestDto stockRequestDto)
 		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
+			var stock = await _service.AddAsync(_mapper.Map<Stock>(stockRequestDto));
+			var stocksDto = _mapper.Map<CreateStockRequestDto>(stock);
+			return Ok(CustomResponseDto<CreateStockRequestDto>.Success(201, stocksDto));
+		}
 
-			var stockModel = _mapper.Map<Stock>(stockRequestDto);
-			await _stockRepository.CreateAsync(stockModel);
-			return Ok(stockRequestDto);
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> Delete([FromRoute] int id)
+		{
+			var stock = await _service.GetByIdAsync(id);
+			await _service.RemoveAsync(stock);
+			return Ok(CustomResponseDto<NoContentDto>.Success(200));
 		}
 
 
@@ -76,25 +62,18 @@ namespace Finance.API.Controllers
 		[Route("{id:int}")]
 		public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateStockRequestDto updateStockRequestDto)
 		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
-
-			var stockModel = await _stockRepository.UpdateAsync(id, updateStockRequestDto);
-			if (stockModel == null) return NotFound();
-			return Ok(updateStockRequestDto);
-
-			//_mapper.Map<Stock>(updateStockRequestDto); değişkene atamadığım için kullanmıyorum
-			//_mapper.Map(updateStockRequestDto, stockModel);
+			var stock = await _service.GetByIdAsync(id);
+			await _service.UpdateAsync(_mapper.Map(updateStockRequestDto, stock));
+			return Ok(CustomResponseDto<NoContentDto>.Success(200));
 		}
 
-		[HttpDelete]
-		[Route("{id:int}")]
-		public async Task<IActionResult> Delete([FromRoute] int id)
-		{
-			if (!ModelState.IsValid) return BadRequest(ModelState);
 
-			var stockModel = await _stockRepository.DeleteAsync(id);
-			if (stockModel == null) return NotFound();
-			return NoContent();
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetById([FromRoute] int id)
+		{
+			var stock = await _service.GetByIdAsync(id);
+			var stockDto = _mapper.Map<StockDto>(stock);
+			return Ok(CustomResponseDto<StockDto>.Success(201, stockDto));
 		}
 	}
 }
