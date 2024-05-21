fec10b5cf4aa7040e07912c2592bc7b943a18930
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-21 15:30:49 +0300

    Migrate filtering and sorting logic to service layer
    - Moved filtering and sorting logic from StockRepository to StockService.
    - Implemented `GetStocksAsync` method in `StockService` to handle filtering, sorting, and pagination based on `QueryObject`.
    - Updated `IStockService` interface to include `GetStocksAsync` method.
    - Ensured proper separation of concerns by keeping the repository layer focused on data access operations.
    - Refactored the repository layer to remove business logic and moved it to the service layer for better maintainability and readability.

--- Finance.Service/Services/StockService.cs
+++ Finance.Service/Services/StockService.cs
@@ -5,6 +5,8 @@ using Finance.Core.Models;
 using Finance.Core.Repositories;
 using Finance.Core.Services;
 using Finance.Core.UnitOfWorks;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Finance.Service.Services
 {
@@ -13,12 +15,13 @@ namespace Finance.Service.Services
 
 		private readonly IStockRepository _stockRepository;
 		private readonly IMapper _mapper;
+		private readonly IFMPService _fmpService;
 
-
-		public StockService(IGenericRepository<Stock> repository, IUnitOfWork unitOfWork, IStockRepository stockRepository, IMapper mapper) : base(repository, unitOfWork)
+		public StockService(IGenericRepository<Stock> repository, IUnitOfWork unitOfWork, IStockRepository stockRepository, IMapper mapper, IFMPService fmpService) : base(repository, unitOfWork)
 		{
 			_stockRepository = stockRepository;
 			_mapper = mapper;
+			_fmpService = fmpService;
 		}
 
 		public async Task<CustomResponseDto<List<StockDto>>> GetAllWithCommentsAsync()
@@ -27,15 +30,57 @@ namespace Finance.Service.Services
 			var stoksDto = _mapper.Map<List<StockDto>>(stoks);
 			return CustomResponseDto<List<StockDto>>.Success(200, stoksDto);
 		}
-
-		public Task<CustomResponseDto<StockDto?>> GetBySymbolAsync(string symbol)
+		public async Task<CustomResponseDto<List<StockDto>>> GetStocksAsync(QueryObject queryObject)
 		{
-			throw new NotImplementedException();
+			var query = _stockRepository.GetAll();
+
+			if (!string.IsNullOrWhiteSpace(queryObject.CompanyName))
+			{
+				var companyNameLower = queryObject.CompanyName.ToLower();
+				query = query.Where(x => x.CompanyName.ToLower().Contains(companyNameLower));
+			}
+
+			if (!string.IsNullOrWhiteSpace(queryObject.Symbol))
+			{
+				var symbolLower = queryObject.Symbol.ToLower();
+				query = query.Where(x => x.Symbol.ToLower().Contains(symbolLower));
+			}
+
+			if (!string.IsNullOrWhiteSpace(queryObject.SortBy))
+			{
+				var param = Expression.Parameter(typeof(Stock), "s");
+				var sortExpression = Expression.Lambda<Func<Stock, object>>(Expression.Convert(Expression.Property(param, queryObject.SortBy), typeof(object)), param);
+				query = queryObject.IsDecsending ? query.OrderByDescending(sortExpression) : query.OrderBy(sortExpression);
+			}
+
+			var skipNumber = (queryObject.PageNumber - 1) * queryObject.PageSize;
+			query = query.Skip(skipNumber).Take(queryObject.PageSize);
+
+			var stocks = await query.ToListAsync();
+			var stocksDto = _mapper.Map<List<StockDto>>(stocks);
+			return CustomResponseDto<List<StockDto>>.Success(200, stocksDto);
 		}
 
-		public Task<bool> StockExist(int id)
+		public async Task<CustomResponseDto<StockDto?>> GetBySymbolAsync(string symbol)
 		{
-			throw new NotImplementedException();
+			var stoks = await _stockRepository.GetBySymbolAsync(symbol);
+			var stoksDto = _mapper.Map<StockDto>(stoks);
+			return CustomResponseDto<StockDto>.Success(200, stoksDto);
+		}
+
+		public async Task<CustomResponseDto<StockDto>> GetOrAddStockAsync(string symbol)
+		{
+			var stock = await _stockRepository.GetBySymbolAsync(symbol);
+			if (stock == null)
+			{
+				stock = await _fmpService.FindStockBySymbolAsync(symbol);
+				if (stock != null)
+				{
+					await _stockRepository.AddStock(stock);
+				}
+			}
+			var stoksDto = _mapper.Map<StockDto>(stock);
+			return CustomResponseDto<StockDto>.Success(200, stoksDto);
 		}
 
 	}
