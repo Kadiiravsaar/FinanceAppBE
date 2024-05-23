f6781eccaacd755dee7023f8988a808f97a3cc21
kadiiravsaar <kadiiravsar@gmail.com>
2024-05-23 23:02:56 +0300

    Added createdBy property to the StockCommentDto class and assigned the same property within StockCommentDto

--- Finance.Core/DTOs/Comment/StockCommentDto.cs
+++ Finance.Core/DTOs/Comment/StockCommentDto.cs
@@ -5,6 +5,8 @@
 		public int Id { get; set; }
 		public string Title { get; set; } = string.Empty;
 		public string Content { get; set; } = string.Empty;
+
+		public string CreatedBy { get; set; } = string.Empty;
 		public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
 	}
 }
